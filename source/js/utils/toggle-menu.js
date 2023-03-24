78e2f90860eb75ff80da0151709b341939bacdc0
Katya <ekaterina589273@yandex.ru>
2023-03-24 16:53:37 +0800

    Реализован каталог и форма обратной связи

--- source/js/utils/toggle-menu.js
+++ source/js/utils/toggle-menu.js
@@ -6,5 +6,11 @@ export let initMenu = () => {
     navMain.classList.toggle('main-header--open');
     navToggle.classList.toggle('main-header__toggle--open');
     navToggle.classList.toggle('main-header__toggle--close');
+
+    if (navMain.classList.contains('main-header--open')) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'visible'
+    }
   });
 };
